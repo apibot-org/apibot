5e5d4b2237928d349f20454d3b669507d5865df6
Fernando Hurtado <fernandohur@gmail.com>
2017-08-05 19:00:17 +0200

    Fixed prod redirect URL

--- env/prod/cljs/apibot/env.cljs
+++ env/prod/cljs/apibot/env.cljs
@@ -7,9 +7,9 @@
 
 (def auth0-client-id "Yi2Vms52QUMf1Y5CL60JL1293vyes8y2")
 (def auth0-domain "picnictest.eu.auth0.com")
-(def auth0-redirect-uri "https://picnictest.eu.auth0.com/mobile")
+(def auth0-redirect-uri "https://app.apibot.co")
 (def auth0-audience "https://api.apibot.co")
 
 (def sentry-dsn "https://d2ccc1a99cd94154a219f663c75cf2c9:031f0f6f228945f280d08a73ef0768a5@sentry.io/192134")
 
-(def apibot-root "https://api.apibot.co/api/1")
+(def apibot-root "https://app.apibot.co/api/1")
